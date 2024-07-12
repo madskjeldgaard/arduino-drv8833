fac132d94045516f917a4b47d9a1ed4b5de09f57
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2024-07-12 22:11:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# DRV8833 Arduino Library
+
+This library provides an interface for controlling the DRV8833 dual H-bridge motor driver chip using the Arduino framework. The DRV8833 can drive two DC motors or one stepper motor. Albeit this currently is mostly written with DC motors in mind.
+
+The library is set up as a platformio library.
+
+## Features
+
+- Platformio library
+- Control two DC motors or one stepper motor.
+- Set the speed and direction of each motor either using integers or floats.
+- Set the decay mode of each H-bridge (fast or slow) to improve performance of brushed DC motors.
+- Put the DRV8833 to sleep to save power.
+- Wake the DRV8833 from sleep.
+
+## Usage
+
+Include the library in your Arduino sketch:
+
+```cpp
+#include "DRV8833.h"
+```
+
+Create an instance of the `DRV8833` class:
+
+```cpp
+motor::DRV8833 motorDriver(in1, in2, in3, in4, sleep);
+```
+
+Control a motor:
+
+```cpp
+// 25% speed, forward
+motorDriver.getBridgeA().setSpeed(0.25, motor::Direction::Forward);
+
+// 13% speed, backwards
+motorDriver.getBridgeA().setSpeed(0.13, motor::Direction::Backward);
+
+```
+
+Put the DRV8833 to sleep:
+
+```cpp
+motorDriver.sleep();
+```
+
+Wake the DRV8833 from sleep:
+
+```cpp
+motorDriver.wake();
+```
+
+## Decay Modes
+
+The decay mode affects the performance of the motor. Fast decay mode causes a rapid reduction in inductive current and allows the motor to coast toward zero velocity. Slow decay mode leads to a slower reduction in inductive current but produces rapid deceleration.
+
+[This Adafruit article has some nice info on performance and DC motors which explains the topic in depth](https://learn.adafruit.com/improve-brushed-dc-motor-performance/overview).
+
+It is also recommended to mess with lowering the PWM frequency of the board to improve performance:
+
+```cpp
+constexpr auto PWM_FREQ = 100; // hz
+analogWriteFreq(PWM_FREQ);
+```
+
+## Installation
+
+### Platformio
+
+Add the library to your lib_deps in `platformio.ini`
+
+```
+lib_deps = 
+    # ... other libraries...
+
+    # DRV8833 motor controller
+    https://github.com/madskjeldgaard/arduino-drv8833
+```
+
+### Arduino
+
+Download the library and add it to your Arduino libraries folder.
+
+## Contributing
+
+All contributions are welcome. Please open an issue or a pull request if you have ideas to change this library for the better :)
+
+## License
+
+This library is released under the MIT license.
---
+++ include/DRV8833.h
@@ -0,0 +1,315 @@
+/*
+ *
+ * This file represents a DRV8833 motor driver chip.  The DRV8833 is a dual
+ * H-bridge motor driver that can drive two DC motors or one stepper motor.
+ *
+ * See the datasheet here:
+ * https://www.ti.com/lit/ds/symlink/drv8833.pdf
+ *
+ *
+ */
+
+#pragma once
+#include <Arduino.h>
+
+namespace motor {
+
+// TODO: Make this consistent across boards
+// The maximum PWM value of the current boards' PWM pins
+// #if defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_ESP32) || \
+//      defined(ARDUINO_ARCH_SAM) ||              \
+//     defined(ARDUINO_ARCH_SAMD)
+// constexpr uint16_t MAX_PWM_VAL = 1023;
+// // These are 255
+// #elif defined(ARDUINO_ARCH_ESP8266)
+// constexpr uint16_t MAX_PWM_VAL = 255;
+// #else
+// constexpr uint16_t MAX_PWM_VAL = 255;
+// #endif
+constexpr uint16_t MAX_PWM_VAL = 255;
+
+/**
+ * @brief The decay mode of the motor driver bridge.
+ * @description The decay mode affects the performance of the motor. Fast decay
+ * mode causes a rapid reduction in inductive current and allows the motor to
+ * coast toward zero velocity. Slow decay mode leads to a slower reduction in
+ * inductive current but produces rapid deceleration.
+ *
+ * Slow decay works well with DC motors.
+ *
+ * See
+ * https://e2e.ti.com/support/motor-drivers-group/motor-drivers/f/motor-drivers-forum/251780/drv8833-pwm-control/881196#881196
+ *
+ */
+enum class DecayMode {
+  Slow = 0,
+  Fast = 1,
+};
+
+/**
+ * @brief The direction of the motor.
+ */
+enum class Direction {
+  Forward,
+  Backward,
+};
+
+/**
+ * @class DRV8833_HBridge
+ * @brief One bridge of a DRV8833 motor driver chip. This repsentation of a
+ * bridge supports fast and slow decay modes.
+ *
+ */
+class DRV8833_HBridge {
+public:
+  // Constructor
+  // in1 and in2 are the pins connected to the motor
+  // The default decay mode is slow
+  /**
+   * @brief Default constructor for the DRV8833_HBridge class. The default
+   * decay mode is slow.
+   *
+   * @param in1 pin1 of the motor driver bridge
+   * @param in2 pin2 of the motor driver bridge
+   */
+  DRV8833_HBridge(uint8_t in1, uint8_t in2) : mIn1(in1), mIn2(in2) {
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    mDecayMode = DecayMode::Slow;
+  }
+
+  /**
+   * @brief Constructor for the DRV8833_HBridge class.  The decay mode can be
+   * specified.
+   *
+   * @param in1 pin1 of the motor driver bridge
+   * @param in2 pin2 of the motor driver bridge
+   * @param mode Decaymode of the motor driver bridge
+   */
+  DRV8833_HBridge(uint8_t in1, uint8_t in2, DecayMode mode)
+      : mIn1(in1), mIn2(in2), mDecayMode(mode) {
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+  }
+
+  /**
+   * @brief Set the decay mode of the motor driver bridge.
+   *
+   * @param mode The decay mode of the motor driver bridge.
+   */
+  void setDecayMode(DecayMode mode) { mDecayMode = mode; }
+
+  void setSpeed(float speed, Direction dir) {
+
+    if (speed > 1.0) {
+      speed = 1.0;
+    } else if (speed < -1.0) {
+      speed = -1.0;
+    }
+
+    const auto pwm_val = static_cast<uint16_t>(speed * MAX_PWM_VAL);
+
+    setSpeed(pwm_val, dir);
+  }
+
+  /*
+   * @brief Set the speed of the motor. This defaults to forward direction
+   *
+   * @param speed The speed of the motor.  The speed is a number between -1.0 and 1.0.
+   *
+   */
+  void setSpeed(float speed) {
+    auto direction = Direction::Forward;
+
+    setSpeed(speed, direction);
+  }
+
+
+  /*
+   * @brief Set the speed and direction of the motor bipolar mode
+   * @param speed The speed of the motor.  The speed is a number between -1.0 and 1.0.
+   * Positive values are forward, negative values are backward, and 0 is stopped.
+   *
+   */
+  void setSpeedBipolar(float speed) {
+    if (speed > 0) {
+      setSpeed(speed, Direction::Forward);
+    } else if (speed < 0) {
+      setSpeed(speed, Direction::Backward);
+    } else {
+      stop();
+    }
+  }
+
+  /**
+   * @brief Set speed. This version of the method assumes the motor is moving in
+   * the forward direction.
+   *
+   * @param speed The speed of the motor. The speed is a number between 0 and
+   * MAX_PWM_VAL (probably 255)
+   */
+  void setSpeed(uint16_t speed) { setSpeed(speed, Direction::Forward); }
+
+  /**
+   * @brief Set speed and direction of the motor
+   *
+   * @param speed The speed of the motor.
+   * @param dir The direction of the motor.  Forward or Backwards.
+   */
+  void setSpeed(uint16_t motorSpeed, Direction dir) {
+
+    // FIXME: Is this a bug?
+    // For some reason, the value is inverted when the decay mode is slow
+    auto speed =
+        mDecayMode == DecayMode::Slow ? MAX_PWM_VAL - motorSpeed : motorSpeed;
+
+    switch (mDecayMode) {
+    case DecayMode::Fast:
+
+      switch (dir) {
+      case Direction::Forward:
+        analogWrite(mIn1, speed);
+        digitalWrite(mIn2, 0);
+        break;
+      case Direction::Backward:
+        digitalWrite(mIn1, 0);
+        analogWrite(mIn2, speed);
+        break;
+      }
+      break;
+    case DecayMode::Slow:
+
+      switch (dir) {
+      case Direction::Forward:
+
+        digitalWrite(mIn1, HIGH);
+        analogWrite(mIn2, speed);
+        break;
+      case Direction::Backward:
+        analogWrite(mIn1, speed);
+        digitalWrite(mIn2, HIGH);
+        break;
+      }
+
+      break;
+    }
+  }
+
+  /**
+   * @brief Set the speed and direction of the motor
+   *
+   * @param speed The speed of the motor, between -MAX_PWM_VAL(probably 255)
+   * and MAX_PWM_VAL. Positive
+   * values are forward, negative values are backward, and 0 is stopped.
+   *
+   */
+  void setSpeedBipolar(int16_t speed) {
+    if (speed > 0) {
+      setSpeed(speed, Direction::Forward);
+    } else if (speed < 0) {
+      setSpeed(speed, Direction::Backward);
+    } else {
+      stop();
+    }
+  }
+
+  void stop() {
+    digitalWrite(mIn1, LOW);
+    digitalWrite(mIn2, LOW);
+  }
+
+private:
+  uint8_t mIn1, mIn2;
+
+  DecayMode mDecayMode;
+};
+
+/**
+ * @class DRV8833
+ * @brief Represents a DRV8833 motor driver with two H-bridges and a sleep pin.
+ * Each H Bridge may be configured in a fast or slow decay mode.
+ *
+ * For more information about decay modes and their impact on performance, see
+ * this article:
+ * https://learn.adafruit.com/improve-brushed-dc-motor-performance/overview
+ *
+ */
+/**
+ * @class DRV8833
+ * @brief A class to control a DRV8833 chip. It has two H-bridges and a sleep
+ * pin to put the chip to sleep.
+ *
+ */
+class DRV8833 {
+public:
+  /**
+   * @brief Default constructor for the DRV8833 class.  The default decay mode
+   * is slow.
+   *
+   * @param in1 Pin connected to AIN1 on the DRV8833
+   * @param in2 Pin connected to AIN2 on the DRV8833
+   * @param in3 Pin connected to BIN1 on the DRV8833
+   * @param in4 Pin connected to BIN2 on the DRV8833
+   * @param sleep Pin connected to SLEEP on the DRV8833
+   */
+  DRV8833(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4, uint8_t sleep)
+      : mBridgeA(in1, in2), mBridgeB(in3, in4), mSleep(sleep) {
+    pinMode(sleep, OUTPUT);
+    digitalWrite(sleep, HIGH);
+  }
+
+  /**
+   * @brief Constructor for the DRV8833 class. The decay mode can be specified.
+   *
+   * @param in1 Pin connected to AIN1 on the DRV8833
+   * @param in2 Pin connected to AIN2 on the DRV8833
+   * @param in3 Pin connected to BIN1 on the DRV8833
+   * @param in4 Pin connected to BIN2 on the DRV8833
+   * @param sleep Pin connected to SLEEP on the DRV8833
+   * @param mode Decay mode of the motor driver chip
+   */
+  DRV8833(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4, uint8_t sleep,
+          DecayMode mode)
+      : mBridgeA(in1, in2, mode), mBridgeB(in3, in4, mode), mSleep(sleep) {
+    pinMode(sleep, OUTPUT);
+    digitalWrite(sleep, HIGH);
+  }
+
+  /**
+   * @brief Stop all connected motors
+   */
+  void stopAll() {
+    mBridgeA.stop();
+    mBridgeB.stop();
+  }
+
+  /**
+   * @brief Get the H-bridge object for the A side of the DRV8833
+   *
+   * @return DRV8833_HBridge&
+   */
+  auto &getBridgeA() { return mBridgeA; }
+
+  /**
+   * @brief Get the H-bridge object for the B side of the DRV8833
+   *
+   * @return DRV8833_HBridge&
+   */
+  auto &getBridgeB() { return mBridgeB; }
+
+  /**
+   * @brief Put the DRV8833 to sleep.
+   */
+  void sleep() { digitalWrite(mSleep, LOW); }
+
+  /**
+   * @brief Wake the DRV8833 from sleep.
+   */
+  void wake() { digitalWrite(mSleep, HIGH); }
+
+private:
+  DRV8833_HBridge mBridgeA, mBridgeB;
+  uint8_t mSleep;
+};
+
+} // namespace motor
